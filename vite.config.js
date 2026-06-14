1dfa4d4b62490cccdf92e4e1c124eba13cdd9465
ainxpro6 <ainxpro6@gmail.com>
2026-06-14 09:43:48 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // 👈 1. Tambahkan import ini
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // ADD THIS LINE (replace with your actual github repo name)
-  base: '/update-info-stok/', 
+  plugins: [
+    react(),
+    tailwindcss(), // 👈 2. Tambahkan ini di dalam kurung siku plugins
+  ],
+  base: '/update-info-stok/',
 })
